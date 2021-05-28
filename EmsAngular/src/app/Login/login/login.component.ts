1e75c9eb39e33e7012d4ea141c92100ff0195b34
donroyj95 <58359628+donroyj95@users.noreply.github.com>
2021-05-28 13:53:12 +0530

    login changes

--- EmsAngular/src/app/Login/login/login.component.ts
+++ EmsAngular/src/app/Login/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {ActivatedRoute, Router} from "@angular/router";
-import {ApiService} from "../../Service/api.service";
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-login',
@@ -10,50 +11,9 @@ import {ApiService} from "../../Service/api.service";
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
-
-
-  constructor(
-
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private apiservice: ApiService
-
-  ) {}
+  constructor() { }
 
   ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-
   }
-
-  // tslint:disable-next-line:typedef
-  get f() { return this.loginForm.controls; }
-
-  // tslint:disable-next-line:typedef
-  onSubmit() {
-    this.submitted = true;
-    // stop here if form is invalid
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    this.apiservice.login(this.f.username.value, this.f.password.value).subscribe(
-        data => {
-          this.router.navigate([this.returnUrl]);
-        },
-        error => {
-          this.loading = false;
-        });
-  }
-
-
 
 }
